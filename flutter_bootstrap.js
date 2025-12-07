a94eda0b20a128103c54b1427a74841b393c792e
hawkinf <hawkinf@gmail.com>
2025-12-07 18:16:24 -0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3858245543"
+    serviceWorkerVersion: "1725068527"
   }
 });
