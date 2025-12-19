6727e30831b7d3f69610bd2b2e26985c26c4f831
Aguinaldo Liesack Baptistini <hawkinf@gmail.com>
2025-12-19 13:32:15 -0300

    Aumentar altura do SizedBox para 280 para resolver overflow completamente

--- lib/main.dart
+++ lib/main.dart
@@ -1499,7 +1499,7 @@ class _HolidayScreenState extends State<HolidayScreen> with SingleTickerProvider
                                       .toList(),
                                 ),
                                 SizedBox(
-                                  height: 250,
+                                  height: 280,
                                   child: GridView.builder(
                                     shrinkWrap: true,
                                     physics: const NeverScrollableScrollPhysics(),
